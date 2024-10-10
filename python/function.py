9faecf841baf5eaa179fa21bc83b9acee7f0ff61
Amin <notreal@mail.com>
2024-10-10 01:46:35 +0000

    Python session 1

---
+++ python/function.py
@@ -0,0 +1,10 @@
+
+def sum(a, b):
+    result = a + b 
+    return result
+
+args1 = int(input("Please enter a number: "))
+args2 = int(input("Please enter second number: "))
+
+result = sum(args1, args2)
+print (result)
---
+++ python/helloworld.py
@@ -0,0 +1,12 @@
+
+messages = "hellow"
+print(messages)
+
+for item in ["mohamad", "Amin", "shirin"]:
+    print(item)
+
+x = 0
+while x < 5:
+    print("Welcome", x, "times") 
+    x += 1
+
